fb3ab1572c8863356e0f2ea228277734fc41f195
Quinten <q.m.b@hotmail.com>
2022-09-08 21:15:30 +0200

    shit flask

--- code/app/app.py
+++ code/app/app.py
@@ -1,19 +1,3 @@
-from flask import Flask
-from io import StringIO
-import csv
-from flask import make_response
+from app import app
 
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route("/csv", methods=['GET'])
-def csv():
-    return 'hi'
-
-if __name__ == "__main__":
-    app.run()
+app.run()
